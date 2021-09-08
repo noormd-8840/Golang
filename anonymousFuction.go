e5dc4657089c5f35609a444214b0b178e0a1df2b
Noor Mohammad Ansari <72457873+noormd-8840@users.noreply.github.com>
2021-09-08 12:25:22 +0530

    Add files via upload

---
+++ anonymousFuction.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+type Employee struct {
+	EmpName string
+}
+
+func (emp Employee) test(){
+	fmt.Println(emp.EmpName)
+}
+
+func main(){
+
+	emp := Employee{"Alex"}
+	emp.test()
+}
