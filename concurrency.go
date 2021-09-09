940b968cc4424eff81b8b32f77066da850d7a073
Noor Mohammad Ansari <72457873+noormd-8840@users.noreply.github.com>
2021-09-09 14:20:49 +0530

    Add files via upload

---
+++ concurrency.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main(){
+	
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// go count("sheep")
+	// go count("Fish")
+
+	go func ()  {
+		count("sheep")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func count(thing string){
+	for i := 1; i<=5; i++{
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond*500)
+	}
+}
